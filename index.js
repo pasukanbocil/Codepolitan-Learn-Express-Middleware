e5821c9c109dd6391df4d93cda8f91aedf4b5118
pasukanbocil <ctian8787@gmail.com>
2024-09-27 10:02:04 +0700

    Refactor middleware in index.js

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const app = express();
 morgan = require("morgan");
 
 app.use(morgan("dev"));
-
+app.use((req, res, next) => {
+  console.log("Hello Middleware Pertama");
+  next();
+  console.log("Hello Middleware Pertama Seteleh next()");
+});
+app.use((req, res, next) => {
+    console.log("Hello Middleware kedua");
+    next();
+  });
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/halaman", (req, res) => {
-    res.send("Hello Halaman");
-  });
+  res.send("Hello Halaman");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
